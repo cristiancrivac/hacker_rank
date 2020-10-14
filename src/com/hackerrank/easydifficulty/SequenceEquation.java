cb554908808a41059e3b20b0238e486161589664
cristian <ccristian.crivac@gmail.com>
2020-10-14 21:44:17 +0300

    Added SequenceEquation.java

---
+++ src/com/hackerrank/easydifficulty/SequenceEquation.java
@@ -0,0 +1,22 @@
+package com.hackerrank.easydifficulty;
+
+import java.util.Scanner;
+
+public class SequenceEquation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] p_inverse = new int[n + 1];
+
+        for (int x = 1; x <= n; x++) {
+            int px = scanner.nextInt();
+            p_inverse[px] = x;
+        }
+
+        for (int x = 1; x <= n; x++) {
+            int y = p_inverse[p_inverse[x]];
+            System.out.println(y);
+        }
+    }
+}
